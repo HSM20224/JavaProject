c0d96aaa85c1f5feca9519629d998bbae5ea8ce7
USER <USER@INET-23>
2018-03-13 18:50:39 +0900

    20180313

---
+++ Java Project/src/Date.java
@@ -0,0 +1,11 @@
+
+public class Date {
+	int year;
+	int month;
+	int day;
+	String month2;
+	
+	void print2(){
+		System.out.println(month2 +" "+day+", " +year);
+	}
+}
---
+++ Java Project/src/imdb.java
@@ -0,0 +1,16 @@
+
+public class imdb {
+	int Year;
+	String Title;
+	float Rating;
+	String ReleaseDate;
+	String Genre;
+	String Time;
+	String Director;
+	String Writer;
+	String Stars;
+	
+	void print() {
+		System.out.println("Year :" +Year+ ", Title :"+Title+ ", Rating :" +Rating+ ", ReleaseDate :"+ReleaseDate+ ",\n Genre :"+Genre+", Time :" +Time+ ", Director :" +Director+ ",\n Writer :" +Writer+ ", Stars : " +Stars  ); 
+	}
+}
