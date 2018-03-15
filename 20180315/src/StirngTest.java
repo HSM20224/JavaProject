3cfb8b4c7a8f8a5c9afd75fa35599312c80f21f0
USER <USER@INET-29>
2018-03-15 19:12:18 +0900

    20180315

---
+++ 20180315/src/StirngTest.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class StirngTest {
+	public static void main(String a[]){
+		String str;
+		Scanner sc = new Scanner(System.in);
+		while (true) {
+			System.out.print("문자열을 입력하세요>");
+			str = sc.next();
+			if (str.equals("quit") == true)
+				break;
+			System.out.println(str);
+		}
+	}
+}
